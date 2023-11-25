304f32462a7faad399dbedff56f02346616ae5ab
ne0nV <117318940+ne0nV@users.noreply.github.com>
2023-11-25 15:31:18 -0500

    Update ubuntu_hardening.sh
    Added null/blank pw check, verify/modify sudoers file, default Umask, PAM hardening, insecure service check.

--- ubuntu_hardening.sh
+++ ubuntu_hardening.sh
@@ -26,6 +26,42 @@ manage_sudo_users() {
     done
 }
 
+# Function to check and modify sudoers file
+check_and_modify_sudoers() {
+    echo "Verifying and modifying /etc/sudoers and /etc/sudoers.d..."
+
+    # Check and modify /etc/sudoers
+    if grep -Eqs 'NOPASSWD|!authenticate' /etc/sudoers; then
+        echo "Found 'NOPASSWD' or '!authenticate' in /etc/sudoers. Removing..."
+        sed -i '/NOPASSWD/d' /etc/sudoers
+        sed -i '/!authenticate/d' /etc/sudoers
+    fi
+
+    # Check and modify files in /etc/sudoers.d
+    for file in /etc/sudoers.d/*; do
+        if grep -Eqs 'NOPASSWD|!authenticate' "$file"; then
+            echo "Found 'NOPASSWD' or '!authenticate' in $file. Removing..."
+            sed -i '/NOPASSWD/d' "$file"
+            sed -i '/!authenticate/d' "$file"
+        fi
+    done
+}
+
+# Function to set default umask for all users
+set_default_umask() {
+    echo "Checking and setting default umask for all users..."
+
+    local current_umask=$(grep "^UMASK" /etc/login.defs | awk '{print $2}')
+
+    if [ "$current_umask" != "077" ]; then
+        echo "Setting umask to 077 in /etc/login.defs..."
+        sed -i '/^UMASK/ s/[0-9]\{3\}/077/' /etc/login.defs
+    else
+        echo "Default umask is already set to 077."
+    fi
+}
+
+
 # Update and Upgrade the System
 echo "Updating and Upgrading the System..."
 apt-get update && apt-get upgrade -y
@@ -70,7 +106,44 @@ harden_ssh() {
 
 # Disable Unused Network Services
 echo "Disabling unused network services..."
-# systemctl disable <service-name>
+# Function to check and remove a service
+remove_service() {
+    local service_name=$1
+    echo "Checking for $service_name service..."
+    if systemctl is-enabled --quiet "$service_name"; then
+        echo "Disabling and removing $service_name..."
+        systemctl stop "$service_name"
+        systemctl disable "$service_name"
+        apt-get remove --purge -y "$service_name"
+    else
+        echo "$service_name is not enabled or installed."
+    fi
+}
+
+# Function to replace FTP with SFTP or FTPS
+replace_ftp() {
+    echo "Checking for FTP service..."
+    if systemctl is-enabled --quiet "vsftpd"; then
+        echo "Removing FTP service..."
+        systemctl stop "vsftpd"
+        systemctl disable "vsftpd"
+        apt-get remove --purge -y "vsftpd"
+        echo "FTP service removed. Consider configuring SFTP or FTPS as a secure alternative."
+    else
+        echo "FTP service is not enabled or installed."
+    fi
+}
+
+# Check for Insecure Services
+remove_service "rsh-server"
+remove_service "telnetd"
+remove_service "apache2"
+remove_service "nginx"
+remove_service "lighttpd"
+remove_service "nis"
+remove_service "sendmail"
+remove_service "postfix"
+replace_ftp
 
 # User Management: Example - Disable guest account
 echo "Disabling guest account..."
@@ -94,5 +167,60 @@ echo "set superusers=\"root\"" >> /etc/grub.d/40_custom
 echo "password_pbkdf2 root $GRUB_HASH" >> /etc/grub.d/40_custom
 update-grub
 
-echo "System hardening and basic administration tasks are complete."
+# Apply SSH Hardening
+harden_ssh
 
+
+
+# Secure PAM Configuration
+secure_pam() {
+    echo "Securing PAM..."
+
+    # Ensure password complexity and strength
+    apt-get install libpam-pwquality -y
+    sed -i '/pam_pwquality.so/ s/^#//' /etc/pam.d/common-password
+    sed -i '/pam_pwquality.so/ s/retry=3/minlen=12 retry=3 difok=3/' /etc/pam.d/common-password
+
+    # Other PAM security measures can be added here
+}
+
+# Apply PAM Hardening
+secure_pam
+
+#Apply Blank/Null Password Check
+check_blank_passwords
+
+# Check for Blank or Null Passwords
+check_blank_passwords() {
+    echo "Checking for blank or null passwords..."
+    local users_with_blank_passwords=$(awk -F: '($2 == "" || $2 == "!") { print $1 }' /etc/shadow)
+
+    for user in $users_with_blank_passwords; do
+        echo "User '$user' has a blank or null password."
+        echo "Choose an action for this account: [C]hange password, [L]ock account, [I]gnore?"
+        read -r action
+        case "$action" in
+            [Cc]* )
+                passwd "$user"
+                ;;
+            [Ll]* )
+                passwd -l "$user"
+                echo "Account $user locked."
+                ;;
+            * )
+                echo "No action taken for $user."
+                ;;
+        esac
+    done
+}
+
+# Apply Blank/Null Password Check
+check_blank_passwords
+
+# Verify and Modify Sudoers File
+check_and_modify_sudoers
+
+# Set Default Umask for All Users
+set_default_umask
+
+echo "System hardening and basic administration tasks are complete."
